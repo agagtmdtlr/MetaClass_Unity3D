b286813bb0ec3781c9512451a7de4f8e3e399395
seungDev <55948026+agagtmdtlr@users.noreply.github.com>
2025-04-02 09:11:17 +0900

    refactoring by copilot

--- Assets/3.Script/Effect/BloodEffectPool.cs
+++ Assets/3.Script/Effect/BloodEffectPool.cs
@@ -7,7 +7,7 @@ using UnityEngine.Pool;
 public class BloodEffectPool : MonoBehaviour
 {
     public GameObject bloodEffectPrefab;
-    ObjectPool<Poolable> effectPool;
+    private ObjectPool<Poolable> effectPool;
 
     private void Start()
     {
@@ -17,42 +17,41 @@ public class BloodEffectPool : MonoBehaviour
             OnReleaseBloodEffect,
             OnDestroyBloodEffect,
             false, 10, 100);
-        
+
         CombatSystem.Instance.Events.OnCombatEvent += ShowBloodEffect;
     }
 
-    public void ShowBloodEffect(CombatEvent combatEvent)
-    {
-        Poolable obj = effectPool.Get();
-        BloodEffect effect = obj as BloodEffect;
-        effect.transform.localRotation = Quaternion.identity;
-        effect.transform.localPosition = Vector3.one;
-        effect.transform.position = combatEvent.HitPosition;
-    }    
     private void OnDisable()
     {
         effectPool.Dispose();
         CombatSystem.Instance.Events.OnCombatEvent -= ShowBloodEffect;
     }
-    
+
+    public void ShowBloodEffect(CombatEvent combatEvent)
+    {
+        Poolable obj = effectPool.Get();
+        if (obj is BloodEffect effect)
+        {
+            effect.transform.localRotation = Quaternion.identity;
+            effect.transform.localPosition = Vector3.one;
+            effect.transform.position = combatEvent.HitPosition;
+        }
+        else
+        {
+            Debug.LogWarning("Invalid effect type");
+        }
+    }
+
     private Poolable CreateBloodEffect()
     {
         Poolable p = Instantiate(bloodEffectPrefab, Vector3.zero, Quaternion.identity).GetComponent<Poolable>();
         p.Pool = effectPool;
         return p;
     }
-    private void OnGetBloodEffect(Poolable bloodEffect)
-    {
-        bloodEffect.gameObject.SetActive(true);
-    }
-    private void OnReleaseBloodEffect(Poolable bloodEffect)
-    {
-        bloodEffect.gameObject.SetActive(false);
-    }
-        
-    private void OnDestroyBloodEffect(Poolable bloodEffect)
-    {
-        Destroy(bloodEffect.gameObject);
-    }
 
+    private void OnGetBloodEffect(Poolable bloodEffect) => bloodEffect.gameObject.SetActive(true);
+
+    private void OnReleaseBloodEffect(Poolable bloodEffect) => bloodEffect.gameObject.SetActive(false);
+
+    private void OnDestroyBloodEffect(Poolable bloodEffect) => Destroy(bloodEffect.gameObject);
 }
